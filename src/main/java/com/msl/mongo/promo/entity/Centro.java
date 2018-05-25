89fce7410413d6ec81b329e677b70cef4d6d4bf3
msalas <msalas@vectoritcgroup.com>
2018-05-25 09:50:29 +0200

    idfile generator, limpio imports

--- src/main/java/com/msl/mongo/promo/entity/Centro.java
+++ src/main/java/com/msl/mongo/promo/entity/Centro.java
@@ -1,10 +1,7 @@
 package com.msl.mongo.promo.entity;
 
-import java.util.List;
-
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
-import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="centro")
